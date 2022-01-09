c3459a70ccbf59bb5320a87380605c47e3e5b687
zeeshan khar <zeeshan.khar1@gmail.com>
2022-01-09 18:35:46 +0500

    some lines are added

--- testing.r
+++ testing.r
@@ -1,2 +1,9 @@
 # This is a first line for my R course
 Print ("Helo R from Git Hub")
+print ("Helo R from Git Hub")
+
+namesd = "zeeshankhar"
+Version = "Rstudio - in writing"
+
+print (namesd)
+print("these lines are written in teh RStudio")
